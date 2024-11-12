f04e633addfde0a7f7be6f00091e682faa7d5a66
Kilzev <knyzeviv@gmai.com>
2024-11-12 17:26:53 +0500

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main(){
+    val purchases: Int = 75
+    val gratitude: String ="Благодарим за покупку!"
+    println(purchases)
+    println(gratitude)
+    var numOfWorkers:Int = 2000
+    //println(numOfWorkers)
+    numOfWorkers = 1999
+    println(numOfWorkers)
+
+
+}
