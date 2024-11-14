d2db8137f3032384966fc61e1e88907bdbb4225b
Kilzev <knyzeviv@gmai.com>
2024-11-14 10:47:13 +0500

    KS-2-5 compound-interest-practice-edits-done

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,11 @@
 package org.example.lesson_1
+
 const val secondsInMinute = 60
 const val minutesInHour = 60
 fun main() {
     val totalSeconds = 6480
-    val hours = String.format("%02d",totalSeconds /secondsInMinute/minutesInHour)
-    val minutes = String.format("%02d",totalSeconds % (secondsInMinute*minutesInHour) / minutesInHour)
-    val seconds = String.format("%02d",totalSeconds % secondsInMinute)
-    println("$hours:$minutes:$seconds")
+    val hours = totalSeconds / secondsInMinute / minutesInHour
+    val minutes = totalSeconds % (secondsInMinute * minutesInHour) / minutesInHour
+    val seconds = totalSeconds % secondsInMinute
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
