e1a325eb0e856c74a8f32eaf964491a25965c202
Kilzev <knyzeviv@gmai.com>
2024-11-14 11:13:03 +0500

    KS-2-3 practice-train-travel-time

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
-const val minutesInHour: Int = 60
+
+const val MINUTES_IN_HOUR: Int = 60
 const val hoursInDay: Int = 24
-fun main(){
+fun main() {
     val departureHour: Int = 9
     val departureMinute: Int = 39
     val minutesTravelTime: Int = 457
-    val totalDepartureMinute = departureHour * minutesInHour + departureMinute
+    val totalDepartureMinute = departureHour * MINUTES_IN_HOUR + departureMinute
     val totalArrivalMinute = totalDepartureMinute + minutesTravelTime
-    val arrivalHour = String.format("%02d",totalArrivalMinute / minutesInHour %hoursInDay)
-    val arrivalMinute = String.format("%02d",totalArrivalMinute % minutesInHour)
+    val arrivalHour = String.format("%02d", totalArrivalMinute / MINUTES_IN_HOUR % hoursInDay)
+    val arrivalMinute = String.format("%02d", totalArrivalMinute % MINUTES_IN_HOUR)
     println("$arrivalHour:$arrivalMinute")
 }
