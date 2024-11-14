948bc77b9b213688eb5cc935afe433fa11de5437
Kilzev <knyzeviv@gmai.com>
2024-11-14 11:17:57 +0500

    KS-2-4 practice-resources-buff

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,11 @@
 package org.example.lesson_2
 
-fun main(){
+fun main() {
     val crystal: Int = 7
     val iron: Int = 11
-    val bonusCoefficient = 0.2f
-    val bonusCrystal = (crystal * bonusCoefficient).toInt()
-    val bonusIron = (iron * bonusCoefficient).toInt()
+    val bonusCoefficient: Int = 20
+    val bonusCrystal = (crystal * bonusCoefficient / 100)
+    val bonusIron = (iron * bonusCoefficient / 100)
     println(bonusCrystal)
     println(bonusIron)
-
 }
