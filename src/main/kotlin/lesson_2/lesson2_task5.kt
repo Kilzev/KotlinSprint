71abcfa1c40dd346c67d90918df7f070599de725
Kilzev <knyzeviv@gmai.com>
2024-11-14 11:21:35 +0500

    KS-2-5 compound-interest-practice

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,9 +4,9 @@ import kotlin.math.pow
 
 fun main() {
     val clientDeposit: Double = 70_000.0
-    val interestRate: Double = 0.167
+    val interestRate: Double = 16.7
     val investYears: Int = 20
-    val profit: String = String.format("%.3f", clientDeposit * ((1 + interestRate).pow(investYears)))
-    println(profit)
+    val profit: Double = clientDeposit * ((1 + interestRate / 100).pow(investYears))
+    println("%.3f".format(profit))
 
 }
