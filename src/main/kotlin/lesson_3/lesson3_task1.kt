70a9de845f3dde4d3ce70fd816e30a9ac3bf3694
Kilzev <knyzeviv@gmai.com>
2024-11-15 17:59:34 +0500

    KS-3-1-greeting-practice

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = readln()
+    val dayGreeting = "Добрый день, $userName"
+    println(dayGreeting)
+    val nightGreeting = "Добрый вечер, $userName"
+    println(nightGreeting)
+}
