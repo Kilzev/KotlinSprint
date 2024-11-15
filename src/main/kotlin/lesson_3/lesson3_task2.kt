187181b819e39828fc4aa23f8f6f7075b4ad832c
Kilzev <knyzeviv@gmai.com>
2024-11-15 18:08:46 +0500

    KS-3-2-surname-in-marriage

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val name = "Татьяна"
+    var surName = "Андреева"
+    val secondName = "Сергеевна"
+    val ageBeforeMarriage = 20
+    val ageAfterMarriage = 22
+
+    println("$surName $name $secondName $ageBeforeMarriage")
+    surName = "Сидорова"
+    println("$surName $name $secondName $ageAfterMarriage")
+}
