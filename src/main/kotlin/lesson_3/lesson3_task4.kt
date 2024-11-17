f789e9d2c8ba308303737f5f910ee7159bc338c5
Kilzev <knyzeviv@gmai.com>
2024-11-17 19:03:33 +0500

    KS-3-4-chess-movement

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -4,9 +4,9 @@ fun main() {
     var startPosition = "E2"
     var endPosition = "E4"
     var numOfMove = 0
-    println("$startPosition:$endPosition;$numOfMove")
+    println("$startPosition-$endPosition;$numOfMove")
     startPosition = "D2"
     endPosition = "D3"
     numOfMove = 1
-    println("$startPosition:$endPosition;$numOfMove")
+    println("$startPosition-$endPosition;$numOfMove")
 }
