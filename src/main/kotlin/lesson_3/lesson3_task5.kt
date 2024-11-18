f5f227b9fc59bf71819b20072fa4a57bbd2d8f3f
Kilzev <knyzeviv@gmai.com>
2024-11-18 17:47:47 +0500

    KS-3-5-chess-parsing

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,10 +2,8 @@ package org.example.lesson_3
 
 fun main() {
     var playerMove = "D2-D4;0"
-    var startMove = ("${playerMove[0]}${playerMove[1]}")
-    var endMove = ("${playerMove[3]}${playerMove[4]}")
-    var numOfMove = ("${playerMove[6]}")
-    println(startMove)
-    println(endMove)
-    println(numOfMove)
+    val parts = playerMove.split("-", ";")
+    println(parts[0])
+    println(parts[1])
+    println(parts[2])
 }
