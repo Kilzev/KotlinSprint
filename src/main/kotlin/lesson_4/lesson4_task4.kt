36f76727e2d9a44f811f11f5cbdb5d5cd2d2b473
Kilzev <knyzeviv@gmai.com>
2024-11-19 22:41:09 +0500

    KS-4-4-training-day

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,12 +2,13 @@ package org.example.lesson_4
 
 fun main() {
     var dayOfTraining = 5
+    var isEven = dayOfTraining % 2 != 0
     println(
         """
-            Упражнения для рук:${dayOfTraining%2 == 1}
-            Упражнения для ног:${dayOfTraining%2 == 0}
-            Упражнения для спины:${dayOfTraining%2 == 0}
-            Упражнения для пресса:${dayOfTraining%2 == 1}
+            Упражнения для рук:$isEven
+            Упражнения для ног:${!isEven}
+            Упражнения для спины:${!isEven}
+            Упражнения для пресса:${isEven}
         """.trimIndent()
     )
 }
