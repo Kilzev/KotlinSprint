6f4d9cc9f5956ecf597a09d793df3db5952fb875
Kilzev <knyzeviv@gmai.com>
2024-11-20 12:45:16 +0500

    KS-5-4-Heart-of-Gold-login

---
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_5
+
+fun main() {
+    val existingLogin = "Zaphod"
+    val existingPassword = "PanGalactic"
+    println("Введите логин: ")
+    val userLogin = readln()
+    if (existingLogin == userLogin) {
+        println("Введите пароль: ")
+        val userPassword = readln()
+        if (userPassword == existingPassword) {
+            println("Добро пожаловать, Zaphod")
+        } else {
+            println("Доступ запрещен")
+        }
+    } else {
+        println("Необходимо пройти регистрацию")
+    }
+
+
+}
