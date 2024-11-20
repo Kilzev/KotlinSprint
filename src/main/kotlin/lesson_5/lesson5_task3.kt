12e20deea089570849c7e5998366aaeaa0797d4d
Kilzev <knyzeviv@gmai.com>
2024-11-20 12:28:59 +0500

    KS-5-3-lottery-practice

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,15 +1,19 @@
 package org.example.lesson_5
 
-fun main(){
+fun main() {
 
     val firstWinNum = 11
-    val secondWinNum =27
+    val secondWinNum = 27
     println("Введите первое число в диапазоне от о до 42: ")
     val userFirstNum = readln().toInt()
     println("Введите второе число в диапазоне от о до 42: ")
     val userSecondNum = readln().toInt()
 
-    if(userFirstNum == firstWinNum && userSecondNum == secondWinNum){println("Поздравляем! Вы выиграли главный приз! ")}
-    else if(userFirstNum == firstWinNum || userSecondNum == secondWinNum){println("Вы выиграли утешительный приз!")}
-    else{println("Неудача")}
+    if (userFirstNum == firstWinNum && userSecondNum == secondWinNum) {
+        println("Поздравляем! Вы выиграли главный приз! ")
+    } else if (userFirstNum == firstWinNum || userSecondNum == secondWinNum) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача")
+    }
 }
