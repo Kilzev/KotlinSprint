182897baea53c475a0ab3d23e3074a7f6d4ef832
Kilzev <knyzeviv@gmai.com>
2024-11-21 11:26:00 +0500

    KS-5-6-body-mass-index

--- src/main/kotlin/lesson_5/lesson5_task6.kt
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -2,7 +2,7 @@ package org.example.lesson_5
 
 const val SANTIMETERS_IN_METERS = 100
 
-fun main(){
+fun main() {
     println("Введите ваш рост в сантиметрах: ")
     val height = readln().toFloat()
     println("Введите ваш вес в килограммах: ")
@@ -14,10 +14,15 @@ fun main(){
     val maxNumBodyMassIndex = 30
 
 
-    if(bodyMassIndex < minNumBodyMassIndex){println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Недостаточная масса тела")}
-    else if (bodyMassIndex >= minNumBodyMassIndex && bodyMassIndex < normalNumBodyMassIndex ){println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Нормальная масса тела")}
-    else if (bodyMassIndex >= normalNumBodyMassIndex && bodyMassIndex < maxNumBodyMassIndex ){println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Избыточная масса тела")}
-    else{println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Ожирение")}
+    if (bodyMassIndex < minNumBodyMassIndex) {
+        println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Недостаточная масса тела")
+    } else if (bodyMassIndex >= minNumBodyMassIndex && bodyMassIndex < normalNumBodyMassIndex) {
+        println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Нормальная масса тела")
+    } else if (bodyMassIndex >= normalNumBodyMassIndex && bodyMassIndex < maxNumBodyMassIndex) {
+        println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Избыточная масса тела")
+    } else {
+        println("Индекс массы тела - ${String.format("%.2f", bodyMassIndex)}: Ожирение")
+    }
 
 
 }
